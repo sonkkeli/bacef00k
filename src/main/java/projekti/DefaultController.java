3b48095c3c8f7592c09a62ac4f4a827b8aca6b3e
sonkkeli <46299415+sonkkeli@users.noreply.github.com>
2019-04-28 20:06:16 +0300

    Update DefaultController.java

--- src/main/java/projekti/DefaultController.java
+++ src/main/java/projekti/DefaultController.java
@@ -6,7 +6,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class DefaultController {
