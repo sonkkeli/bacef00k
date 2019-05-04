52b6136dbef32f0b08f6803607a5740282b77b7c
sonkkeli <46299415+sonkkeli@users.noreply.github.com>
2019-05-04 14:08:04 +0300

    Update DefaultController.java

--- src/main/java/projekti/controllers/DefaultController.java
+++ src/main/java/projekti/controllers/DefaultController.java
@@ -48,11 +48,11 @@ public class DefaultController {
     @GetMapping("*")
     public String landingPage(Model model) {
         boolean isLoggedIn = false;
-        System.out.println("sisällä tänään " + userSer.loggedInUser());
+        // System.out.println("sisällä tänään " + userSer.loggedInUser());
         if (userSer.loggedInUser() != null){
             isLoggedIn = true;
         }
-        System.out.println(isLoggedIn);
+        // System.out.println(isLoggedIn);
         model.addAttribute("isLoggedIn", isLoggedIn);
         model.addAttribute("uniqueUsername", true);
         model.addAttribute("uniqueProfilename", true);
